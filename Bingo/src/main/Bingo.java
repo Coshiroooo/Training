19adc3a80a1d1c49937c1d5779d12b717fad4887
本山幸志郎 <koshiro-nora@motoyamakoushirounoMacBook-Air.local>
2018-04-10 11:38:21 +0900

    ビンゴゲームのマス目と抽選番号

--- Bingo/src/main/Bingo.java
+++ Bingo/src/main/Bingo.java
@@ -3,13 +3,26 @@ package main;
 import java.util.*;
 
 public class Bingo {
+
+	private int squareLength;
+	private List<Integer> winningNumbers;
 	
-	Bingo(){
+	Bingo(int squareLength){ //コンストラクタ
 		
+		this.squareLength = squareLength;
+		
+		List<Integer> winningNumbers = new ArrayList<Integer>();
+		
+		for(int i = 1; i < 101; i++){
+			winningNumbers.add(i);
+		}
+		Collections.shuffle(winningNumbers);
+		this.winningNumbers = winningNumbers;
 	}
 	
-	public void arrangeNumber() {
-		
+	public int comeNumber() {
+		return this.winningNumbers.get(0);
+//		winningNumbers.remove(0);
 	}
 
 }
