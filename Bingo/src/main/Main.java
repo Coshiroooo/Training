54b12736104902a2a814b87c9c55d3a2bd24b803
本山幸志郎 <koshiro-nora@motoyamakoushirounoMacBook-Air.local>
2018-04-10 16:56:55 +0900

    当選番号に当たるとどんどん空白になるようにしました

--- Bingo/src/main/Main.java
+++ Bingo/src/main/Main.java
@@ -1,12 +1,13 @@
 package main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
 	
-//	static int squareLength = 5;
-	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		
 		Bingo bingo = new Bingo(); //Bingoクラスをインスタンス化
 		
@@ -18,13 +19,34 @@ public class Main {
 		System.out.println();
 		System.out.println("==============================");
 		
+		System.out.println("        Game Start");
 		System.out.println();
-		System.out.println("     当選番号は " + bingo.comeNumber() + " です！！");
-		System.out.println();
 		
-		bingo.fixBingo();
+		for(int i = 0; i < 100; i++) {
+			System.out.println();
+			System.out.println("　　　　  ↓抽選する↓");
+			enter();
+			System.out.println();
+			System.out.println("         " + (i + 1) +"回目の抽選" );
+			System.out.println("     当選番号は " + bingo.comeNumber() + " です！！");
+			System.out.println();
+			
+			bingo.fixBingo();
+		}
 		
 		
+	}
+	
+	public static void enter() throws IOException{ //Enter押すと次の処理が始まる
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		sb.append(br);
+		String enter = sb.toString();
+		System.in.read();
+		switch(enter){
+			case "\r\n":
+				break;
+		}
 	}
 	
 }
