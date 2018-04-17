5e7051ff3ecfeb54dc5a43dec27d30504af98f35
本山幸志郎 <koshiro-nora@motoyamakoushirounoMacBook-Air.local>
2018-04-17 18:53:00 +0900

    ババ抜き作ってみたよ

---
+++ Baba/src/main1/BoneYard.java
@@ -0,0 +1,11 @@
+package main1;
+
+import java.util.*;
+
+public class BoneYard {
+	private List<String> deadCards = new ArrayList<String>();
+	
+	public List<String> getDeadCards(){
+		return this.deadCards;
+	} 
+}
