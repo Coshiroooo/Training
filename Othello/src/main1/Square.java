b9592b301bb4b851fdf12dc0c37adc812a032fc3
本山幸志郎 <koshiro-nora@motoyamakoushirounoMacBook-Air.local>
2018-04-20 11:12:42 +0900

    石を置けるようになった

--- Othello/src/main1/Square.java
+++ Othello/src/main1/Square.java
@@ -5,17 +5,23 @@ public class Square {
 	private String box;
 	private int number;
 	
+	//コンストラクタ
 	Square(int number){
 		this.number = number;
 		this.box = String.format("%02d", this.number);
 	}
 	
+	//石を置くメソッド
 	public void putStone(String stone) {
 		this.box = stone;
 	}
 	
 	public void printSquare() {
 		System.out.print(" " + box + " ");
+	}
+	
+	public String getBox() {
+		return this.box;
 	}
 	
 }
