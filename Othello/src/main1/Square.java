0fec6e82fc6dcc3706f01ca964257cfad9484706
本山幸志郎 <koshiro-nora@motoyamakoushirounoMacBook-Air.local>
2018-04-20 12:46:22 +0900

    aroundSquaresが完成　マスの周りに何があるかが分かるようになったよ

--- Othello/src/main1/Square.java
+++ Othello/src/main1/Square.java
@@ -1,14 +1,23 @@
 package main1;
 
+import java.util.*;
+
 public class Square {
 
 	private String box;
 	private int number;
+	private String state = "valid";
+	private Map<String,Square> aroundSquares = new HashMap<String,Square>();
 	
 	//コンストラクタ
 	Square(int number){
 		this.number = number;
 		this.box = String.format("%02d", this.number);
+	}
+	
+	//コンストラクタのオーバーロード
+	Square(String state){
+		this.state = state;
 	}
 	
 	//石を置くメソッド
@@ -20,8 +29,16 @@ public class Square {
 		System.out.print(" " + box + " ");
 	}
 	
+	public int getNumber() {
+		return this.number;
+	}
+	
 	public String getBox() {
 		return this.box;
+	}
+	
+	public void inputAroundSquares(Map<String,Square> aroundSquares){
+		this.aroundSquares = aroundSquares;
 	}
 	
 }
