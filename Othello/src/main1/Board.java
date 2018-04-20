dd3baff8f6ee441bca26ec44de0cbbbe6fc5fd22
本山幸志郎 <koshiro-nora@motoyamakoushirounoMacBook-Air.local>
2018-04-20 11:12:42 +0900

    石を置けるようになった

--- Othello/src/main1/Board.java
+++ Othello/src/main1/Board.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Board {
 
 	private int width = 8;
-	private String white = "◎️";
+	private String white = "◎";
 	private String black = "◉";
 	private List<List<Square>> boardSquares = new ArrayList<List<Square>>();
 
@@ -18,10 +18,10 @@ public class Board {
 		for (int i = 0; i < Math.pow(width, 2); i += width) {
 			boardSquares.add(squares.subList(i, i + width));
 		}
-		changeSquare(3,3,black);
-		changeSquare(3,4,white);
-		changeSquare(4,3,white);
-		changeSquare(4,4,black);
+		changeSquare(3, 3, black);
+		changeSquare(3, 4, white);
+		changeSquare(4, 3, white);
+		changeSquare(4, 4, black);
 	}
 
 	// ボードの状態を表示する
@@ -32,22 +32,48 @@ public class Board {
 				System.out.print("|");
 				square.printSquare();
 			}
-			System.out.print("|");
-			System.out.println();
+			System.out.println("|");
 		}
 		printLine();
 	}
 
-	//ボードの行線を表示する
+	// ボードの行線を表示する
 	public void printLine() {
 		for (int i = 0; i < width; i++) {
 			System.out.print("-----");
 		}
 		System.out.println();
 	}
-	
-	public void changeSquare(int x,int y,String stone) {
+
+	// 指定した座標に石を置くメソッド
+	public void changeSquare(int x, int y, String stone) {
 		boardSquares.get(y).get(x).putStone(stone);
+	}
+
+	//マス目の番号入力するとそこに石を置いてくれるメソッド
+	public void changeSquare(int squareNumber, String stone) {
+		int y = (int) (squareNumber / width);
+		if (squareNumber % 8 == 0) {
+			y = y - 1;
+		}
+		int x = (squareNumber % width) - 1;
+		if (x == 0) {
+			x = width - 1;
+		}
+		changeSquare(x,y,stone);
+	}
+	
+	//盤上の石の数を数えるメソッド
+	public int countStone(String stone) {
+		int count = 0;
+		for(List<Square> squareLine : boardSquares) {
+			for(Square square : squareLine) {
+				if(square.getBox().equals(stone)) {
+					count++;
+				}
+			}
+		}
+		return count;
 	}
 
 }
