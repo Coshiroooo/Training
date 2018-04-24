d441fcb0076545b727e50537147b831ecf2691b3
本山幸志郎 <koshiro-nora@motoyamakoushirounoMacBook-Air.local>
2018-04-24 14:59:28 +0900

    nullを消した

--- Othello/src/main1/Square.java
+++ Othello/src/main1/Square.java
@@ -4,61 +4,67 @@ import java.util.*;
 
 public class Square {
 
-	private String box;
 	private int number;
 	private String numberS;
-	private Stone stone;
+	private Stone stone = new Stone();
+	private boolean isValid = true;
+	private boolean isShown = true;
 	private Map<String, Square> aroundSquares = new HashMap<String, Square>();
 
 	// コンストラクタ
 	Square(int number) {
 		this.number = number;
 		this.numberS = String.format("%02d", this.number);
-		this.box = this.numberS;
 	}
 
 	// コンストラクタのオーバーロード
 	Square() {
-		this.box = "null";
+		this.isValid = false;
 	}
 
 	// マス目に石が格納されるメソッド
 	public void strageStone(Stone stone) {
 		this.stone = stone;
-		if (this.stone != null) {
-			this.box = this.stone.getFrontColor();
-		}
 	}
 
 	// 隣のマスが違う色の石かを判定するメソッド
-	public Boolean isDifferentColor(String key) {
+	public boolean isDifferentColor(String key) {
 		Square nextSquare = aroundSquares.get(key);
-		if(this.stone != null && nextSquare.getStone() != null) {
+		if(this.stone.getIsExist() && nextSquare.getStone().getIsExist()) {
 			return nextSquare.getStone().getFrontColor().equals(this.stone.getBackColor());
 		}else {
 			return false;
 		}
 	}
+	
+	public void switchIsShown() {
+		this.isShown = !this.isShown;
+	}
 
 	// 石をひっくり返すメソッド
 	public void turnOverStone() {
 		this.stone.turnOver();
-		this.box = this.stone.getFrontColor();
 	}
 
 	// マス目を表示する
 	public void printSquare() {
-		System.out.print(" " + box + " ");
+		if(this.isValid) {
+			if(this.stone.getIsExist()) {
+				System.out.print(" " + this.stone.getFrontColor() + " ");
+			}else {
+				if(this.isShown) {
+				System.out.print(" " + this.numberS + " ");
+				}else {
+					System.out.print(" -- ");
+				}
+			}
+		}
 	}
 
 	//セッター
 	
 	public void setAroundSquares(Map<String, Square> aroundSquares) {
 		this.aroundSquares = aroundSquares;
-	}
-	
-	public void setBox(String box) {
-		this.box = box;
 	}
 
 	//ゲッター
@@ -74,9 +80,9 @@ public class Square {
 	public Stone getStone() {
 		return this.stone;
 	}
-
-	public String getBox() {
-		return this.box;
+	
+	public boolean getIsShown() {
+		return this.isShown;
 	}
 
 	public Map<String, Square> getAroundSquares() {
