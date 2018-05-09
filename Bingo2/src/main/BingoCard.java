14e24d9bfb68b45b62de9ce0a54ad851734ee29b
本山幸志郎 <koshiro-nora@motoyamakoushirounoMacBook-Air.local>
2018-05-09 15:36:01 +0900

    適用できそうなところにstreamを適用させました

---
+++ Bingo2/src/main/BingoCard.java
@@ -0,0 +1,99 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class BingoCard {
+
+	private Scanner scanner = new Scanner(System.in);
+	private List<List<Integer>> bingoCardNumbers = new ArrayList<List<Integer>>(); // カードに配置する数字のList
+	public static List<Integer> cardWidthIndex; // カードの幅のインデックスを並べたList
+	public static int numberMax; // ビンゴカードに書かれうる数字の最大値
+
+	// コンストラクタ
+	BingoCard(int cardWidth) {
+		this.bingoCardNumbers = makeBingoCardNumbers(cardWidth);
+		cardWidthIndex = new ArrayList<Integer>();
+		for (int i = 0; i < cardWidth; i++)
+			cardWidthIndex.add(i);
+	}
+
+	// ビンゴカード上に配置した数字のリストを作るメソッド
+	public static List<List<Integer>> makeBingoCardNumbers(int cardWidth) {
+
+		List<Integer> bingoNumbers = new ArrayList<Integer>();
+		List<List<Integer>> bingoCardNumbers = new ArrayList<List<Integer>>();
+
+		if (cardWidth <= 10) {
+			for (int i = 1; i <= 100; i++)
+				bingoNumbers.add(i);
+		} else {
+			for (int i = 1; i <= cardWidth * cardWidth; i++)
+				bingoNumbers.add(i);
+		}
+
+		numberMax = bingoNumbers.stream()
+								.max((a, b) -> a.compareTo(b))
+								.get();
+
+		Collections.shuffle(bingoNumbers);
+
+		for (int i = 0; i < cardWidth; i++) {
+			bingoCardNumbers.add(new ArrayList<Integer>(bingoNumbers.subList(i * cardWidth, (i + 1) * cardWidth)));
+		}
+
+		return bingoCardNumbers;
+	}
+
+	// 抽選状況に合わせたビンゴカードを作るメソッド
+	public void updateBingo(Lottery lottery, int cardWidth, int count) {
+
+		bingoCardNumbers.forEach(bcNumbers -> {
+			for (int i = 0; i < cardWidth; i++)
+				System.out.print("______");
+			System.out.println();
+			bcNumbers	.stream()
+						.map(b -> lottery	.getCurrentWinningNumbers()
+											.contains(b) ? "|     " : String.format("| %3d ", b))
+						.forEach(System.out::print);
+			System.out.print("|");
+			System.out.println();
+		});
+		for (int i = 0; i < cardWidth; i++)
+			System.out.print("______");
+		System.out.println();
+		System.out.println();
+		System.out.println(bingoCardNumbers	.stream()
+											.anyMatch(b -> b.contains(lottery	.getCurrentWinningNumbers()
+																				.get(count))) ? "当たり！" : "残念！");
+		System.out.println();
+		System.out.println();
+	}
+
+	// 初期ビンゴを作る
+	public void makeBingo(int cardWidth) {
+
+		bingoCardNumbers.forEach(bcNumbers -> {
+			for (int i = 0; i < cardWidth; i++)
+				System.out.print("______");
+			System.out.println();
+			bcNumbers	.stream()
+						.map(b -> String.format("| %3d ", b))
+						.forEach(System.out::print);
+			System.out.print("|");
+			System.out.println();
+		});
+		for (int i = 0; i < cardWidth; i++)
+			System.out.print("______");
+		System.out.println();
+		System.out.println();
+	}
+
+	// ゲッター//
+	public List<List<Integer>> getBingoCardNumbers() {
+		return bingoCardNumbers;
+	}
+
+}
---
+++ Bingo2/src/main/Main.java
@@ -0,0 +1,109 @@
+package main;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+
+	static Lottery lottery = new Lottery();
+	static Scanner scanner = new Scanner(System.in);
+	static int playerNumber;
+	static int cardWidth;
+
+	public static void main(String[] args) throws IOException {
+
+		List<Player> allPlayers = new ArrayList<Player>();
+
+		System.out.println();
+		System.out.println("【BINGO GAME】");
+
+		selectWidth();
+		selectMember();
+
+		for (int i = 1; i <= playerNumber; i++)
+			allPlayers.add(new Player(i, cardWidth));
+
+		System.out.println("Game Start!!");
+
+		allPlayers.forEach(p -> p.makeMyBingoCard(cardWidth));
+
+		int count = 0;
+
+		do {
+			System.out.println("↓抽選する↓");
+			enter();
+
+			pickWinningNumber();
+			printWinning(count);
+
+			for (Player player : allPlayers)
+				player.updateMyBingoCard(lottery, cardWidth, count);
+
+			count++;
+
+		} while (allPlayers	.stream()
+							.noneMatch(p -> p.isBingo(lottery, cardWidth)));
+
+		allPlayers	.stream()
+					.filter(p -> p.isBingo(lottery, cardWidth))
+					.map(p -> p.getPlayerName() + "のビンゴ！！！")
+					.forEach(System.out::println);
+	}
+
+	// ゲーム参加人数を決めるメソッド
+	public static void selectMember() {
+		System.out.println();
+		System.out.print("何人でビンゴしますか？：");
+		playerNumber = scanner.nextInt();
+	}
+
+	// ビンゴのマス目の幅を入力するメソッド
+	public static void selectWidth() {
+		System.out.print("ビンゴのマス目の幅を入力してください：");
+		cardWidth = scanner.nextInt();
+		if (cardWidth > 30) {
+			System.out.println("30以下の数字を入力してください");
+			selectWidth();
+		}
+	}
+
+	// lotteryからランダムに数字を出し、既出もしくは範囲外ならもう一度引く
+	public static void pickWinningNumber() {
+		int winningNumber = lottery.pickNumber();
+		if (winningNumber <= BingoCard.numberMax && !lottery.getCurrentWinningNumbers()
+															.contains(winningNumber)) {
+			lottery	.getCurrentWinningNumbers()
+					.add(winningNumber);
+		} else {
+			pickWinningNumber();
+		}
+	}
+
+	// Enterを押すと次の処理が始まる
+	public static void enter() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		sb.append(br);
+		String enter = sb.toString();
+		System.in.read();
+		switch (enter) {
+		case "\r\n":
+			break;
+		}
+	}
+
+	// 当選番号の表示など
+	public static void printWinning(int count) {
+		System.out.println();
+		System.out.println((count + 1) + "回目の抽選");
+		System.out.println();
+		System.out.println("当選番号は" + lottery.getCurrentWinningNumbers()
+											.get(count)
+				+ "です！");
+	}
+
+}
