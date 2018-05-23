c6be7c3ba296186d9e39eb29572a281e3a97cc72
本山幸志郎 <koshiro-nora@motoyamakoushirounoMacBook-Air.local>
2018-05-23 17:56:24 +0900

    fix__

--- QuizGame/src/main/Main2.java
+++ QuizGame/src/main/Main2.java
@@ -1,75 +1,142 @@
 package main;
 
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.IntStream;
 
-//これまでの結果を表示するクラス
+//問題の追加・削除、履歴の削除などを自由にやるクラス
 public class Main2 {
-
-	private static final DBConnecter quizDB = new DBConnecter("localhost", "quiz_db", "root", "");
+	
+	private static final DBConnecter quizDB = new DBConnecter("localhost","quiz_db","root","");
+	private static final Scanner scan = new Scanner(System.in);
 
 	public static void main(String[] args) {
-
-		printResults();
-
+		selectDeleteQues();
+		selectAddQues();
+		selectDeleteResults();
 	}
-
-	// 結果を表示するメソッド
-	public static void printResults() {
-
-		String sqlResults = "SELECT * FROM results;";
-		List<Integer> resultIds = quizDB.selectColInt(sqlResults, 1);
-		List<LocalDateTime> resultDateTimes = quizDB.selectColDateTime(sqlResults, 2);
-		List<Integer> resultPoints = quizDB.selectColInt(sqlResults, 3);
-
-		System.out.println("\n--------------------");
-		System.out.println("【これまでの結果】\n");
-
-		int count = 0;
-		DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm");
-
-		while (count < resultIds.size()) {
-			System.out.print("第" + String.format("%02d", resultIds.get(count)) + "回");
-			System.out.print("\t" + String.format("%4s", resultPoints.get(count)) + "点");
-			System.out.println("\t[" + resultDateTimes.get(count).format(format) + "]　");
+	
+	//問題を追加するメソッド
+	public static void selectAddQues() {
+		System.out.println("--------------------");
+		System.out.print("問題を追加しますか？(y/n)\n==> :");
+		String select = scan.next();
+		if("y".equals(select)) {
+			
+			printQuesAll();
+			quizDB.update("INSERT INTO questions VALUES()");
+			int quesIdMax = quizDB.selectInt("SELECT MAX(id) FROM questions");
+			
+			System.out.print("設問を入力してください\n==> :");
+			String question = scan.next();
+			System.out.print("選択肢の数を入力してください\n==>：");
+			int choiceNumber = scan.nextInt();
+			IntStream.rangeClosed(1, choiceNumber).boxed().forEach(n -> {
+				System.out.print(n + "番目の選択肢を入力してください\n==> :");
+				String choice = scan.next();
+				quizDB.update("INSERT INTO choices VALUES('" + quesIdMax + "','" + n + "','" + choice + "')");
+			});
+			System.out.print("正解の選択肢を入力してください\n==> :");
+			int correctChoiceId = scan.nextInt();
+			
+			quizDB.update("UPDATE questions SET question = '" + question  + "' , correct_choice_id = " + correctChoiceId + " WHERE id = " + quesIdMax);
+			System.out.println();
+			System.out.println("問題の追加が完了しました\n");
+			printQuesAll();
+			selectAddQues();
+			
+		}else if("n".equals(select)) {
+			System.out.println("問題は追加しません");
+		}else {
+			System.out.println("追加するなら y を、そうでなければ n を入力してください");
+			selectAddQues();
+		}
+	}
+	
+	//問題一覧を表示するメソッド
+	public static void printQuesAll() {
+		System.out.println("--------------------");
+		System.out.println("【問題一覧】\n");
+		int count = 1;
+		List<String> quesList = quizDB.selectColStr("SELECT * FROM questions", 2);
+		for(String ques : quesList) {
+			System.out.println(count + ". " + ques);
 			count++;
 		}
-
-		System.out.println("\n--------------------\n");
-
-		System.out.println("挑戦数：\t" + intToStr("%4s", "SELECT MAX(id) FROM results;") + "回");
-		System.out.println("最高点：\t" + intToStr("%4s", "SELECT MAX(point) FROM results;") + "点");
-		System.out.println("最低点：\t" + intToStr("%4s", "SELECT MIN(point) FROM results;") + "点");
-		System.out.println("平均点：\t" + intToStr("%4s", "SELECT AVG(point) FROM results;") + "点\n");
-
-		System.out.println("【あなたが間違えやすい問題】\n");
-		List<Integer> quesWorstRankIds = quizDB
-				.selectColInt("SELECT id FROM questions WHERE ques_numbers > 0 ORDER BY correct_rate ASC LIMIT 3;", 1);
-
-		int i = 3;
-		Collections.reverse(quesWorstRankIds);
+		System.out.println("--------------------");
+	}
+	
+	//問題削除選択をさせるメソッド
+	public static void selectDeleteQues() {
+		System.out.println("--------------------");
+		System.out.print("問題を削除しますか？(y/n)\n==> :");
+		String select = scan.next();
+		if("y".equals(select)) {
+			printQuesAll();
+			System.out.print("削除する問題のidを入力してください\n==> :");
+			int deleteQuesId = scan.nextInt();
+			deleteQues(deleteQuesId);
+			printQuesAll();
+			selectDeleteQues();
+		}else if("n".equals(select)) {
+			System.out.println("問題は削除しません");
+		}else {
+			System.out.println("削除するなら y を、そうでなければ n を入力してください");
+			selectDeleteQues();
+		}		
+	}
+	
+	//指定されたidの問題を削除するメソッド
+	public static void deleteQues(int deleteQuesId) {
 		
-		for (int id : quesWorstRankIds) {
-			String quesWHERE = "WHERE id = " + id;
-			List<Integer> choiceIds = quizDB.selectColInt("SELECT * FROM choices WHERE question_id = " + id, 2);
-
-			System.out.print("【" + i + "位】\t");
-			System.out.println("正答率：" + intToStr("%3s", "SELECT correct_rate FROM questions " + quesWHERE) + "%");
-			System.out.println("Q. " + quizDB.selectStr("SELECT question FROM questions " + quesWHERE));
-			choiceIds.stream()
-					.map(choiceId -> choiceId + ". " + quizDB.selectStr(
-							"SELECT choice FROM choices WHERE choice_id = " + choiceId + " AND question_id = " + id))
-					.forEach(System.out::println);
-			System.out.println();
-			i--;
+		quizDB.update("DELETE FROM questions WHERE id = " + deleteQuesId);
+		quizDB.update("DELETE FROM choices WHERE question_id = " + deleteQuesId);
+		quizDB.update("ALTER TABLE questions ADD new_id int(11) NOT NULL FIRST");
+		
+		List<Integer> quesIdList = quizDB.selectColInt("SELECT id FROM questions", 1);
+		quesIdList.forEach(id -> quizDB.update("UPDATE questions SET new_id = " + id + " WHERE id = " + id));
+		
+		quizDB.update("UPDATE choices RIGHT JOIN questions ON choices.question_id = questions.id SET question_id = new_id");
+		quizDB.update("ALTER TABLE questions drop column id");
+		quizDB.update("ALTER TABLE questions CHANGE new_id id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT FIRST");
+		quizDB.update("ALTER TABLE questions AUTO_INCREMENT = 1");
+		
+		System.out.println("idが" + deleteQuesId + "の問題を削除しました");
+		
+	}
+	
+	//問題を全削除するメソッド
+	public static void deleteQuesAll() {
+		do {
+			int quesIdMin = quizDB.selectInt("SELECT MIN(id) FROM questions");
+			deleteQues(quesIdMin);
+		}while(quizDB.selectColInt("SELECT id FROM questions", 1).size() > 1);
+	}
+	
+	//履歴を全部消すか選択するメソッド
+	public static void selectDeleteResults() {
+		System.out.println("--------------------");
+		System.out.print("履歴を削除しますか？(y/n)\n==> :");
+		String select = scan.next();
+		if("y".equals(select)) {
+			deleteResultsAll();
+			System.out.println("履歴を全て削除しました");
+		}else if("n".equals(select)) {
+			System.out.println("履歴は削除しません");
+		}else {
+			System.out.println("削除するなら y を、そうでなければ n を入力してください");
+			selectDeleteResults();
 		}
-
 	}
-
-	// int型をformatを揃えたStringで返してくれるメソッド
-	public static String intToStr(String format, String sql) {
-		return String.format(format, String.valueOf(quizDB.selectInt(sql)));
+	
+	//Resultsを全部消すメソッド
+	public static void deleteResultsAll() {
+		quizDB.update("DELETE FROM results");
+		quizDB.update("DELETE FROM logs");
+		quizDB.update("ALTER TABLE results AUTO_INCREMENT = 1");
+		quizDB.update("ALTER TABLE logs AUTO_INCREMENT = 1");
+		quizDB.update("UPDATE questions SET ques_numbers = 0, correct_numbers = 0, correct_rate = 0");
+		System.out.println("全てのResultsを削除しました");
 	}
-
+	
 }
