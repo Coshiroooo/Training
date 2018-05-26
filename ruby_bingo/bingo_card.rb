97fd44f2e25973a640baf679203d3b6c6ecb7ff3
本山幸志郎 <koshiro@Koshiros-MacBook-Pro.local>
2018-05-26 13:33:52 +0900

    ver1.0

---
+++ ruby_bingo/bingo_card.rb
@@ -0,0 +1,53 @@
+class BingoCard
+
+  @@card_width
+  @@numbers_max
+
+  # イニシャライザ
+  def initialize
+    @@numbers_max = @@card_width <= 10 ? 100 : @@card_width ** 2
+    @bingo_numbers = if @@card_width <= 10
+                       [*1..100].shuffle!.slice(0, @@card_width ** 2).each_slice(@@card_width).to_a
+                     else
+                       [*1..@@card_width ** 2].shuffle!.slice(0, @@card_width ** 2).each_slice(@@card_width).to_a
+                     end
+  end
+
+  # 抽選状況に合わせたビンゴを表示するメソッド
+  def create_bingo(count, previous_numbers)
+    @bingo_numbers.each do |line|
+      @@card_width.times {print "+----"}
+      puts "+"
+      line.each do |n|
+        print previous_numbers.include?(n) ? "|    " : "|#{sprintf("%3s", n)} "
+      end
+      puts "|"
+    end
+    @@card_width.times {print "+----"}
+    puts "+\n"
+    if count > 0
+      puts @bingo_numbers.any? {|line| line.include?(previous_numbers[count - 1])} ? "あたり！" : "残念！"
+    end
+  end
+
+  # @@card_widthのセッター
+  def self.get_card_width
+    @@card_width
+  end
+
+  # @@card_widthのセッター
+  def self.set_card_width(card_width)
+    @@card_width = card_width
+  end
+
+  # @@numbers_maxのゲッター
+  def self.get_numbers_max
+    @@numbers_max
+  end
+
+  # @bingo_numbersのセッター
+  def get_bingo_numbers
+    @bingo_numbers
+  end
+
+end
---
+++ ruby_bingo/player.rb
@@ -0,0 +1,35 @@
+require './bingo_card'
+
+class Player
+
+  # イニシャライザ
+  def initialize(number)
+    @bingo_card = BingoCard.new()
+    @name = "Player#{number}さん"
+  end
+
+  # 自分のビンゴカードを表示するメソッド
+  def print_bingo(count, previous_numbers)
+    puts "\n#{@name}のカードです"
+    @bingo_card.create_bingo(count,previous_numbers)
+  end
+
+  # ビンゴを判定するメソッド
+  def bingo?(previous_numbers)
+    bingo_numbers = @bingo_card.get_bingo_numbers
+    card_width = BingoCard.get_card_width
+
+    is_vertical_bingo = [*0..card_width].any? {|index| bingo_numbers.all? {|line| previous_numbers.include?(line[index])}}
+    is_side_bingo = bingo_numbers.any? {|line| line.all? {|n| previous_numbers.include?(n)}}
+    is_slant1_bingo = bingo_numbers.all? {|line| previous_numbers.include?(line[bingo_numbers.index(line)])}
+    is_slant2_bingo = bingo_numbers.all? {|line| previous_numbers.include?(line[card_width - bingo_numbers.index(line) - 1])}
+
+    is_vertical_bingo || is_side_bingo || is_slant1_bingo || is_slant2_bingo
+  end
+
+  # @nameのゲッター
+  def get_name
+    @name
+  end
+
+end
