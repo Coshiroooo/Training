5b28b1eef7cba43d462d6a10439940d63b95e458
本山幸志郎 <koshiro@Koshiros-MacBook-Pro.local>
2018-05-28 14:03:21 +0900

    +accessor

--- ruby_bingo/player.rb
+++ ruby_bingo/player.rb
@@ -2,6 +2,8 @@ require './bingo_card'
 
 class Player
 
+  attr_accessor :name
+
   # イニシャライザ
   def initialize(number)
     @bingo_card = BingoCard.new()
@@ -16,8 +18,8 @@ class Player
 
   # ビンゴを判定するメソッド
   def bingo?(previous_numbers)
-    bingo_numbers = @bingo_card.get_bingo_numbers
-    card_width = BingoCard.get_card_width
+    bingo_numbers = @bingo_card.bingo_numbers
+    card_width = BingoCard.card_width
 
     is_vertical_bingo = [*0..card_width].any? {|index| bingo_numbers.all? {|line| previous_numbers.include?(line[index])}}
     is_side_bingo = bingo_numbers.any? {|line| line.all? {|n| previous_numbers.include?(n)}}
@@ -25,12 +27,6 @@ class Player
     is_slant2_bingo = bingo_numbers.all? {|line| previous_numbers.include?(line[card_width - bingo_numbers.index(line) - 1])}
 
     is_vertical_bingo || is_side_bingo || is_slant1_bingo || is_slant2_bingo
-  end
-
-  # ========================================= getter
-
-  def get_name
-    @name
   end
 
 end
