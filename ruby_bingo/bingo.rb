082722a3789f3bee704d84e1e50198f049e9ae4e
本山幸志郎 <koshiro@Koshiros-MacBook-Pro.local>
2018-05-29 10:27:07 +0900

    runメソッド作った。クラス変数無くした。

--- ruby_bingo/bingo.rb
+++ ruby_bingo/bingo.rb
@@ -2,72 +2,72 @@ require './player'
 
 class Bingo
 
-  @@player_number
-  @@players
-  @@previous_numbers = []
-
   # ビンゴのマス目の幅を入力するメソッド
-  def self.select_width
+  def select_width
     print "\nビンゴのマス目の幅を入力してください："
     card_width = gets.to_i
     BingoCard.card_width = card_width
   end
 
   # ゲームの参加人数を決めるメソッド
-  def self.select_member
+  def select_member
     print "\n何人でビンゴしますか？："
-    @@player_number = gets.to_i
+    @player_number = gets.to_i
   end
 
   # 抽選番号をだすメソッド
-  def self.put_win_number
+  def put_win_number
     win_number = rand(999) + 1
-    if win_number <= BingoCard.numbers_max and !@@previous_numbers.include?(win_number)
-      @@previous_numbers << win_number
+    if win_number <= BingoCard.numbers_max and !@previous_numbers.include?(win_number)
+      @previous_numbers << win_number
     else
       put_win_number
     end
   end
 
   # 当選番号を表示するメソッド
-  def self.print_win_number(count)
+  def print_win_number(count)
     puts "#{count}回目の抽選"
-    puts "\n当選番号は#{@@previous_numbers[count - 1]}です！"
+    puts "\n当選番号は#{@previous_numbers[count - 1]}です！"
   end
 
   # キーボード入力すると次の処理が始まるメソッド
-  def self.key_input
+  def key_input
     begin
       gets
     rescue
     end
   end
 
-  # ========================================= ここから実行
+  # クラスを実行するメソッド
+  def run
+    puts "【BINGO GAME】"
 
-  puts "【BINGO GAME】"
+    select_width
+    select_member
 
-  select_width
-  select_member
+    puts "Game Start!!"
 
-  puts "Game Start!!"
+    count = 0
+    @previous_numbers = []
 
-  count = 0
+    @players = [*1..@player_number].map { |n| Player.new(n)}
+    @players.each {|p| p.print_card(count,@previous_numbers)}
 
-  @@players = [*1..@@player_number].map { |n| Player.new(n)}
-  @@players.each {|p| p.print_card(count,@@previous_numbers)}
+    loop do
+      count += 1
+      puts "\n↓抽選する↓"
+      key_input
+      put_win_number
+      print_win_number(count)
+      @players.each {|player| player.print_card(count,@previous_numbers)}
+      @players.any?{|player| player.bingo?(@previous_numbers)} ? break : next
+    end
 
-  loop do
-    count += 1
-    puts "\n↓抽選する↓"
-    key_input
-    put_win_number
-    print_win_number(count)
-    @@players.each {|player| player.print_card(count,@@previous_numbers)}
-    @@players.any?{|player| player.bingo?(@@previous_numbers)} ? break : next
+    winner = @players.find {|p| p.bingo?(@previous_numbers)}
+    puts "\n#{winner.name}のビンゴ！！！"
   end
 
-  winner = @@players.find {|p| p.bingo?(@@previous_numbers)}
-  puts "\n#{winner.name}のビンゴ！！！"
-
 end
+
+Bingo.new.run
